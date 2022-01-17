b6d3026e76594fda9cb68ea662ff7b2b085898a8
zeybetcak <63880441+zeybetcak@users.noreply.github.com>
2022-01-16 17:21:23 -0800

    updated gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,11 @@ function App() {
 <div className="grid-container">
   <header className="row">
     <div>
-      <a className="brand" href="index.html">Silk Road Home</a>
+      <a className="brand" href="/">Silk Road Home</a>
     </div>
     <div>
-      <a href="cart.html">Cart</a>
-      <a href="signin.html">Sign In</a>
+      <a href="/cart">Cart</a>
+      <a href="/signin">Sign In</a>
     </div>
   </header>
   <main>
