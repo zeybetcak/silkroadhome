b1ba18a6de86ad6173c3933c7342f066b230844e
zeybetcak <63880441+zeybetcak@users.noreply.github.com>
2022-01-17 12:30:03 -0800

    created a components folder and added a product.js and rating.js file to render the star based ratings and showcase the individual products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Product from './components/Product';
 import data from './data';
 
 function App() {
@@ -15,38 +16,9 @@ function App() {
   </header>
   <main>
  <div className="row center">
-{
-  data.products.map(product => (
-    <div key={product._id} className="card">
-    <a href={`/product/${product._id}`}>
-      <img className="medium" src={product.image} alt={product.name}/>
-    </a>
-    <div className="card-body"> 
-    <a href={`/product/${product._id}`}>
-      <h2>{product.name}</h2>
-      </a>
-      <div className="rating">
-        <span>
-          <i className="fa fa-star"></i>
-        </span>
-        <span>
-          <i className="fa fa-star"></i>
-        </span>
-        <span>
-          <i className="fa fa-star"></i>
-        </span>
-        <span>
-          <i className="fa fa-star"></i>
-        </span>
-        <span>
-          <i className="fa fa-star-half-o"></i>
-        </span>
-      </div>
-      <div className="price">${product.price}</div>
-    </div>
-   </div>
-  ))
-}
+{data.products.map((product) => (
+    <Product key={product._id} product={product}></Product>
+  ))}
  </div>
   </main>
   <footer className="row center">
