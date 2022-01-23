b3bfd82a50fa277229ca9f09d8bb4747de46fc38
zeybetcak <63880441+zeybetcak@users.noreply.github.com>
2022-01-23 02:03:17 -0800

    changed all a to link in app.js and product.js next to cart added a badge with styling in index.css, in store.js added a new reducer, saved the add to cart action in local storage, when products are added to cart the number of items in cart changes

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,8 +1,8 @@
 import Axios from 'axios';
 import { CART_ADD_ITEM } from "../constants/cartConstants";
 
-export const addToCart = (productId, qty) => async(dispatch, getState) => {
-  const {data} = await Axios.get(`/api/product/${productId}`);
+export const addToCart = (productId, qty) => async (dispatch, getState) => {
+  const { data } = await Axios.get(`/api/products/${productId}`);
   dispatch({
     type: CART_ADD_ITEM,
     payload: {
@@ -14,4 +14,5 @@ export const addToCart = (productId, qty) => async(dispatch, getState) => {
       qty, 
     },
   });
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
 };
