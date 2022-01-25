cfee553fa2bfeec0c86588bfd4075bbd403ecfe5
zeybetcak <63880441+zeybetcak@users.noreply.github.com>
2022-01-24 23:26:15 -0800

    started building cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -7,6 +7,8 @@ function CartScreen(props) {
   const qty = props.location.search
   ? Number(props.location.search.split('=')[1])
   : 1;
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
   const dispatch = useDispatch();
   useEffect(() => {
   if (productId) {
@@ -14,12 +16,31 @@ function CartScreen(props) {
   }
   }, [dispatch, productId, qty]);
   return (
-  <div>
-    <h1>Cart Screen</h1>
-      <p> ADD TO CART : ProductID: {productId} Qty: {qty} 
-      </p>
+<div className = "row top"> 
+<div className="col-2">
+  <h1> Shopping Cart </h1>
+  { cartItems.length === 0? <MessageBox> 
+  Cart is empty. <Link to ="/"> Go Shopping </Link>
+  </MessageBox>
+  ) : ( 
+    <ul>
+    {cartItems.map((item) => (
+    <li key= {item.product}>
+      <div className="row">
+        <div> 
+          <img 
+          src = {item.image}
+          alt = {item.name}
+          className= "small">
+          </img>
+        </div>
   </div>
-  );
-}
+  </li>
+    ))}
+    </ul>
+  </div>
+  )}
+  </div>
+  }
 
 export default CartScreen;
