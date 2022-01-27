6bb6af4d02e2332e5ea3b76db7d8853b162f89bb
zeybetcak <63880441+zeybetcak@users.noreply.github.com>
2022-01-26 23:20:20 -0800

    implemented the delete button for items in cart, so shopper can remove items not wanted by defining a cart action, then in reducer used the filter method to remove product with action.payload id and lastly used dispatch in cartScreen.js

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 import MessageBox from '../components/MessageBox';
 
  
@@ -19,8 +19,8 @@ function CartScreen(props) {
   }
   }, [dispatch, productId, qty]);
   const removeFromCartHandler = (id) => {
-    // delete action
-  };
+    dispatch(removeFromCart(id));
+  }
   const checkoutHandler = () => {
     props.history.push('/signin?redirect=shipping');
   }
