07b44069f93b29a39bab12288a785262f155b5fc
zeybetcak <63880441+zeybetcak@users.noreply.github.com>
2022-01-28 23:49:19 -0800

    debugging the userRouter and server.js to try to load users on API

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,14 @@ import data from './data.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/silkroadhome', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/silkroadhome', { useNewUrlParser:true,
+useUnifiedTopology:true,
+useCreateIndex:true}).then(()=>{
+console.log(`successfully connected`);
+}).catch(( err )=>{
+console.log(`not connected`);
+})
+
 
 app.get('/api/products/:id', (req, res) => {
  const product = data.products.find(x => x._id === req.params.id);
@@ -17,8 +21,6 @@ app.get('/api/products/:id', (req, res) => {
    res.status(404).send({ message: 'Product not Found' });
  }
 });
-
-
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
