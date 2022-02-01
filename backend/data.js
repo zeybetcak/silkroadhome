ec55eb7375f1bd8c44299efd1563f199fedb0326
zeybetcak <63880441+zeybetcak@users.noreply.github.com>
2022-01-31 23:28:42 -0800

    created a seed api for products, and added 5 products to the database in mongodb

--- backend/data.js
+++ backend/data.js
@@ -17,7 +17,6 @@ const data = {
   ],
   products: [
     {
-      _id: '1',
       name: '100% Linen Turkish Towel',
       category: 'Towels',
       image: '/images/p1.jpg',
@@ -29,7 +28,6 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '2',
       name: '100% Linen Turkish Hand Towel',
       category: 'Towels',
       image: '/images/p2.jpg',
@@ -41,7 +39,6 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '3',
       name: '100% Linen Turkish Pajama Set',
       category: 'Clothing',
       image: '/images/p3.jpg',
@@ -53,7 +50,6 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '4',
       name: '100% Cotton Turkish Bathrobe',
       category: 'Clothing',
       image: '/images/p4.jpg',
@@ -65,7 +61,6 @@ const data = {
       description: 'high quality product',
     },
     {
-      _id: '5',
       name: 'Turkish Takunya Bath Sandals',
       category: 'Shoes',
       image: '/images/p5.jpg',
@@ -77,6 +72,6 @@ const data = {
       description: 'high quality product',
     },
   ],
-};
+}; 
 
 export default data;
---
+++ backend/routers/productRouter.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import data from '../data.js';
+import Product from '../models/productModel.js';
+
+const productRouter = express.Router();
+
+productRouter.get(
+'/seed', 
+expressAsyncHandler(async (req,res) => {
+const createdProducts = await Product.insertMany(data.products);
+res.send({ createdProducts });
+})
+);
+export default productRouter;
